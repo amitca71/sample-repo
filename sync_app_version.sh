f1df563eed855ec85a8d2ecf1b81c8b2f449844c
amitca71 <amitca71@gmail.com>
2021-06-13 18:16:54 +0300

    add comments to sync_app_version.sh

--- sync_app_version.sh
+++ sync_app_version.sh
@@ -1,8 +1,11 @@
-#!/usr/bin/env bash -x
+#!/usr/bin/env bash
+#get version update_type argument (default=patch)
 version_update_type=${1:-patch}
 pid=$$
+#in case of error log the errors to temp file
 npm_info=$(npm info --json 2>/tmp/err${pid})
 if [ $? -ne 0 ]; then
+    #checks if the error was because there is no such npm registry
     grep "is not in the npm registry" /tmp/err${pid}
     if [ $? -eq 0 ]; then
         echo "the package is not yet published to npm, no need to update lates version"
@@ -12,23 +15,24 @@ if [ $? -ne 0 ]; then
         exit 1
     fi
 fi
-#app_version=$(echo $npm_info |  tr '\r\n' ' ' | jq '."versions"' | tr '\r\n[]' ' ' | tr -d " " | cut -d'"' -f2)
 export NPM_INF=$npm_info
 #backup original package.json before replacing
 cp package.json /tmp/package.json_${pid}
 jsonFile="package.json"
+export JSON_FILE=$jsonFile
 node <<EOF
 //Read orig file
 var data = require('./${jsonFile}');
 var fs = require('fs');
 //get the repository info
 var npm_info=JSON.parse(process.env.NPM_INF);
-var fileName='package.json'
+//var fileName='package.json'
+var fileName=process.env.JSON_FILE
+//get versions list from npm_info
 var versions=npm_info['versions'];
 //assign latest version to new file
 data.version=versions[versions.length-1];
 //Output data
-console.log(data);
 fs.writeFile(fileName, JSON.stringify(data, null, 2), function writeJSON(err) {
   if (err) return console.log(err);
   console.log(JSON.stringify(data));
